b8997285110a49e25d9746ff20b1b9955a1739ad
Maxime Thibault <maximethibault1@gmail.com>
2020-04-06 07:55:28 -0400

    add sample classification based on thresholding and output to text file

--- evaluate.py
+++ evaluate.py
@@ -40,10 +40,12 @@ warnings.filterwarnings('ignore',category=UserWarning)
 # loaded. Must be a subdirectory of "experiments". Will save there.
 # LOAD_TEST_DATA_FROM specifies where to find the test set data.
 # Must be a subdirectory of "preprocessed_data".
-LOAD_MODEL_FROM = 'retrospective-gan/testfullmodel'
-LOAD_TEST_DATA_FROM = '20yr_byyear'
+LOAD_MODEL_FROM = 'retrospective-gan/94- 80%l1 100-2-1 selu single run novalid abstractfinal'
+LOAD_TEST_DATA_FROM = '2yr_byyear_test'
 # List of years to use for validation set in 'year' SPLIT_MODE
-VALID_YEARS = [2017]
+VALID_YEARS = [2018]
+# Encoder loss threshold for atypical profile determination
+ENCODER_LOSS_THRESHOLD = 0.48157547911008197
 
 save_path = os.path.join('experiments', LOAD_MODEL_FROM)
 with open(os.path.join(save_path, 'hp.pkl'), mode='rb') as file:
@@ -153,7 +155,7 @@ eval_generator = TransformedGenerator(param.MODE, w2v_step, param.USE_LSI, pse, 
 #%%
 n = neural_network(param.MODE)
 if param.MODE in ['retrospective-autoenc', 'retrospective-gan']:
-	custom_objects_dict = {'autoencoder_accuracy':n.autoencoder_accuracy, 'autoencoder_false_neg_rate':n.autoencoder_false_neg_rate}
+	custom_objects_dict = {'autoencoder_accuracy':n.autoencoder_accuracy, 'autoencoder_false_neg_rate':n.autoencoder_false_neg_rate, 'combined_l1l2loss':n.combined_l1l2loss}
 else:
 	custom_objects_dict = {'sparse_top10_accuracy': n.sparse_top10_accuracy, 'sparse_top30_accuracy': n.sparse_top30_accuracy}
 
@@ -206,11 +208,12 @@ else:
 # ### Compute and print evaluation metrics
 
 #%%
-print('Evaluation on test set results')
-print('Predictions for {} classes'.format(len(le.classes_)))
-print('{} classes reprensented in targets'.format(len(set(chain.from_iterable(filtered_targets)))))
-for metric, result in zip(model.metrics_names, results):
-	print('Metric: {}   Score: {:.5f}'.format(metric,result))
+with open(os.path.join(save_path,'evaluation_results.txt'), mode='w', encoding='cp1252') as file:
+	file.write('Evaluation on test set results\n')
+	file.write('Predictions for {} classes\n'.format(len(le.classes_)))
+	file.write('{} classes reprensented in targets\n'.format(len(set(chain.from_iterable(filtered_targets)))))
+	for metric, result in zip(model.metrics_names, results):
+		file.write('Metric: {}   Score: {:.5f}\n'.format(metric,result))
 
 #%%
 if param.MODE in ['retrospective-autoenc', 'retrospective-gan']:
@@ -232,7 +235,8 @@ else:
 	plt.savefig(os.path.join(save_path, 'predictions_distribution.png'))
 plt.gcf().clear()
 nb_predicted_classes = len(pl_series.value_counts().index)
-print('Number of classes predicted on evaluation set: {}'.format(nb_predicted_classes))
+with open(os.path.join(save_path,'evaluation_results.txt'), mode='a', encoding='cp1252') as file:
+	file.write('Number of classes predicted on evaluation set: {}\n'.format(nb_predicted_classes))
 
 #%%
 
@@ -240,8 +244,9 @@ cr = classification_report(y_true, y_pred, output_dict=True)
 cr_df = pd.DataFrame(cr).transpose()
 cr_df.to_csv(os.path.join(save_path,  'eval_classification_report.csv'))
 p_we, r_we, _, _ = precision_recall_fscore_support(y_true, y_pred, average='micro')
-print('Micro average precision score for present labels: {:.3f}'.format(p_we))
-print('Micro average recall score for present labels: {:.3f}'.format(r_we))
+with open(os.path.join(save_path,'evaluation_results.txt'), mode='a', encoding='cp1252') as file:
+	file.write('Micro average precision score for present labels: {:.3f}\n'.format(p_we))
+	file.write('Micro average recall score for present labels: {:.3f}\n'.format(r_we))
 
 #%%
 department_data = pd.read_csv(os.path.join(os.getcwd(), 'data', 'depas.csv'), sep=';')
@@ -257,12 +262,20 @@ unique_categorized_departments = list(set(categorized_departments))
 #%%
 # Compute evaluation metrics by department as a proxy for patient
 # category. Make a dataframe with this.
-if param.MODE in ['retrospective-autoenc', 'retrospective-gan']:
+if param.MODE == 'retrospective-autoenc':
 	results_by_depa_dict = {
 		'Department':[],
 		'Area under precision-recall': [],
 		'Autoencoder accuracy': [],
 		'Autoencoder false negative rate': [],
+	}
+elif param.MODE == 'retrospective-gan':
+	results_by_depa_dict = {
+		'Department':[],
+		'Area under precision-recall': [],
+		'Autoencoder accuracy': [],
+		'Autoencoder false negative rate': [],
+		'Atypical profiles ratio': [],
 	}
 else:
 	results_by_depa_dict = {
@@ -277,7 +290,8 @@ else:
 for department in unique_categorized_departments:
 	if department == 'null':
 		continue
-	print('\n\nResults for category: {}'.format(department))
+	with open(os.path.join(save_path,'evaluation_results.txt'), mode='a', encoding='cp1252') as file:
+		file.write('\n\nResults for category: {}\n'.format(department))
 	indices = [i for i, value in enumerate(categorized_departments) if value == department]
 	if param.MODE == 'retrospective':
 		selected_pre_seqs = [filtered_pre_seqs[i] for i in indices]
@@ -298,6 +312,11 @@ for department in unique_categorized_departments:
 	if len(selected_targets) < 10:
 		continue
 	eval_generator = TransformedGenerator(param.MODE, w2v_step, param.USE_LSI, pse, le, selected_targets, selected_pre_seqs, selected_post_seqs, selected_active_meds, selected_active_classes, selected_depa, param.W2V_EMBEDDING_DIM, param.SEQUENCE_LENGTH, param.BATCH_SIZE)
+	sample_eval_generator = TransformedGenerator(param.MODE, w2v_step, param.USE_LSI, pse, le, selected_targets, selected_pre_seqs, selected_post_seqs,
+											selected_active_meds, selected_active_classes, selected_depa, param.W2V_EMBEDDING_DIM, param.SEQUENCE_LENGTH, len(selected_targets))
+	batch_data_X_val, batch_data_y_val = sample_eval_generator.__getitem__(0)
+	samples_X_val = batch_data_X_val['pse_input']
+	samples_y_val = batch_data_y_val['main_output']
 	if param.MODE == 'retrospective-gan':
 		g_eval_losses = []
 		g_eval_predictions = []
@@ -316,11 +335,31 @@ for department in unique_categorized_departments:
 		results = np.array(g_eval_losses)
 		predictions = np.vstack(g_eval_predictions)
 		results = np.mean(g_eval_losses, axis=0)
+
+		# Compute the encoder loss distribution
+
+		print('SAMPLING ENCODER LOSSES ON THE DEPARTMENT...')
+		g_val_sampled_losses = []
+		
+		for sample_x, sample_y in tqdm(zip(samples_X_val, samples_y_val)):
+			ones_label = np.ones(1)
+			latent_repr = gan_encoder.predict(sample_x)
+			reconst = gan_decoder.predict(latent_repr)
+			feature_extracted = gan_feature_extractor.predict(reconst)
+
+			g_loss = model.test_on_batch(sample_x.reshape(1,-1), [sample_y.reshape(1,-1), feature_extracted, ones_label, latent_repr])
+
+			g_val_sampled_losses.append(g_loss)
+		
+		g_val_sampled_losses = np.array(g_val_sampled_losses)
+		encoder_val_losses = g_val_sampled_losses[:,[4]].squeeze()
+		atypical_profiles_ratio = np.sum((encoder_val_losses > ENCODER_LOSS_THRESHOLD).astype(int)) / len(encoder_val_losses)
+
 	else:
 		results = model.evaluate_generator(eval_generator, verbose=1)
 		predictions = model.predict_generator(eval_generator, verbose=1)
 	if param.MODE in ['retrospective-autoenc', 'retrospective-gan']:
-		if param.MODE == 'retrosective-autoenc':
+		if param.MODE == 'retrospective-autoenc':
 			aupr_index = 2
 			accuracy_index = 1
 			false_neg_index = 3
@@ -334,21 +373,26 @@ for department in unique_categorized_departments:
 		results_by_depa_dict['Area under precision-recall'].append(results[aupr_index])
 		results_by_depa_dict['Autoencoder accuracy'].append(results[accuracy_index])
 		results_by_depa_dict['Autoencoder false negative rate'].append(results[false_neg_index])
+		if param.MODE == 'retrospective-gan':
+			results_by_depa_dict['Atypical profiles ratio'].append(atypical_profiles_ratio)
 		prediction_label_matrix = (predictions >= 0.5) * 1
 		prediction_labels = le.inverse_transform(prediction_label_matrix)
 		p_we, r_we, _, _ = precision_recall_fscore_support(le.transform(selected_targets), prediction_label_matrix, average='micro')
-		print('Micro average precision score for present labels: {:.3f}'.format(p_we))
-		print('Micro average recall score for present labels: {:.3f}'.format(r_we))
-		print('\n\n\n')
-		print('Sampled predictions:\n')
-		sampled_indices = random.sample(range(len(selected_targets)), 10)
-		sampled_true = [[definitions[med] for med in profile] for profile in [selected_targets[i] for i in sampled_indices]]
-		sampled_pred = [[definitions[med] for med in profile] for profile in [prediction_labels[i] for i in sampled_indices]]
+		with open(os.path.join(save_path,'evaluation_results.txt'), mode='a', encoding='cp1252') as file:
+			file.write('Micro average precision score for present labels: {:.3f}\n'.format(p_we))
+			file.write('Micro average recall score for present labels: {:.3f}\n'.format(r_we))
+			file.write('\n\n\n')
+			file.write('Sampled predictions:\n')
+		sampled_indices = random.sample(range(len(selected_targets)), 50)
+		sampled_true = [[definitions[med] if med in definitions.keys() else '' for med in profile] for profile in [selected_targets[i] for i in sampled_indices]]
+		sampled_pred = [[definitions[med] if med in definitions.keys() else '' for med in profile if med not in profile_pred] for profile, profile_pred in zip([selected_targets[i] for i in sampled_indices], [prediction_labels[i] for i in sampled_indices])]
 		for true, pred in zip(sampled_true, sampled_pred):
 			true.sort()
 			pred.sort()
-			print('\nTRUE PROFILE: \n{} \nPREDICTED PROFILE:{}\n'.format(true, pred))
-		print('\n\n\n')
+			with open(os.path.join(save_path,'evaluation_results.txt'), mode='a', encoding='cp1252') as file:
+				file.write('\nTRUE PROFILE: \n{} \nATYPICALS:{}\n'.format(true, pred))
+		with open(os.path.join(save_path,'evaluation_results.txt'), mode='a', encoding='cp1252') as file:
+			file.write('\n\n\n')
 	else:
 		# Compute the baseline predictions by frequency-based prediction
 		s = pd.Series(selected_targets)
